1e6e56a0d6061865c6ed46ce1e3b92b5d5a13707
Benji Brana <benjidebran@gmail.com>
2024-03-30 01:03:01 +0100

    ajout

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -22,14 +22,29 @@ const articles = [
 
 export default function MySlider() {
   const [page, setPage] = useState(1);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setPage((prev) => (prev === articles.length ? 1 : prev + 1));
-    }, 5000);
-
-    return () => clearInterval(interval);
+    // Vérifier si toutes les images sont chargées
+    const images = articles.map(
+      (article) => (new Image().src = article.image)
+    );
+    Promise.all(images)
+      .then(() => setImagesLoaded(true))
+      .catch((error) =>
+        console.error('Erreur lors du chargement des images', error)
+      );
   }, []);
+
+  useEffect(() => {
+    if (imagesLoaded) {
+      const interval = setInterval(() => {
+        setPage((prev) => (prev === articles.length ? 1 : prev + 1));
+      }, 5000);
+
+      return () => clearInterval(interval);
+    }
+  }, [imagesLoaded]);
 
   const handlePagePrecedante = () => {
     setPage((prev) => (prev === 1 ? articles.length : prev - 1));
@@ -38,6 +53,10 @@ export default function MySlider() {
   const handlePageSuivante = () => {
     setPage((prev) => (prev === articles.length ? 1 : prev + 1));
   };
+
+  if (!imagesLoaded) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <section className="relative flex h-[calc(100vh-70px)] w-full flex-col justify-center items-center bg-primary">
