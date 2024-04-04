382c33efa682dc00fac2db573ada041c21e9d1d2
Benji Brana <benjidebran@gmail.com>
2024-04-04 14:53:01 +0200

    ajout fil ariane

--- src/components/FilAriane/FilAriane.jsx
+++ src/components/FilAriane/FilAriane.jsx
@@ -1,5 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FilAriane() {
   const [segments, setSegments] = useState([]);
@@ -9,11 +11,28 @@ export default function FilAriane() {
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-row mt-6 ml-6">
       {segments.map((path, index) => (
-        <div id="filAriane" key={index}>
-          <span>{path !== '' ? path : 'accueil'}</span>
-          {index !== segments.length - 1 && <span>/</span>}
+        <div className="flex flex-row" id="filAriane" key={index}>
+          <span>
+            {path !== '' ? (
+              path
+            ) : (
+              <Link href="/">
+                <Image
+                  className="rounded-full tablette:w-1/2 mobile:w-1/3  "
+                  src="/logo/home.svg"
+                  alt="Logo home"
+                  title="Logo retour accueil"
+                  width={25}
+                  height={25}
+                />
+              </Link>
+            )}
+          </span>
+          {index !== segments.length - 1 && (
+            <span className="mx-2">/</span>
+          )}
         </div>
       ))}
     </div>
